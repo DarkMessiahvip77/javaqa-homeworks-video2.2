54719e972f77ebd4a2571e2d35d057d1c64a24e6
“Ilya” <mitsukiv@yandex.ru>
2023-04-19 21:22:34 +0300

    add src/

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         int client = 1100;
         int summ = check + client;
         int bonus; // client / 100
-        if ( client >= 1000) {
+        if (client >= 1000) {
             bonus = summ + (client + check) / 100;
             System.out.println(" Всего c бонусами= " + bonus);
         } else {
